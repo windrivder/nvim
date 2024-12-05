17380bef924cc7aa861fee91f72af3c929774448
weiyuanyuan1 <weiyuanyuan1@360.cn>
2024-12-05 12:38:19 +0800

    feat: frontend code format

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -6,6 +6,17 @@ autocmd("BufEnter", {
   command = "set fo-=c fo-=r fo-=o",
 })
 
+-- auto indent and space
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "javascript", "typescript", "html", "css", "xml", "json", "yaml", "vue" },
+  callback = function()
+    vim.opt_local.expandtab = true
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.tabstop = 2
+  end,
+})
+
 -- dont list quickfix buffers
 autocmd("FileType", {
   pattern = "qf",
